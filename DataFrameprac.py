8279f3b4ace0e38e49374433432746e57cd95d84
erinseepersad <105820206+erinseepersad@users.noreply.github.com>
2023-07-19 13:22:12 -0400

    Add files via upload

--- DataFrameprac.py
+++ DataFrameprac.py
@@ -63,3 +63,90 @@ exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Micha
 'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
 'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
 labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+df= pd.DataFrame(exam_data, index= labels)
+#print(df.iloc['attempts']>2)
+print (df[df['attempts']>2]) #the whole dataframe, then the attempts dataframe inside that
+
+#8
+exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+df= pd.DataFrame(exam_data, index= labels)
+#print(df.count())
+#rows
+num_rows = 0
+for i in df.iterrows():
+    num_rows += 1
+
+#columns
+num_columns = 0
+for i in df.columns:
+    num_columns += 1
+print("Number of rows:", num_rows)
+print("Number of columns:", num_columns)
+
+
+#9
+exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+#labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+df= pd.DataFrame(exam_data, index= labels)
+print(df[df['score'].isna()])
+
+"""
+if df['score'].isna().any():
+    missing_scores = df[df['score'].isna()]
+    print(missing_scores)
+else:
+    print("No missing scores found.")
+"""
+#10
+exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+df= pd.DataFrame(exam_data, index= labels)
+#print(df.iloc[14:19]) #exculsive
+selected_rows = df[(df['score'] >= 15) & (df['score'] <= 20)]
+print(selected_rows)          
+
+#11
+exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+df= pd.DataFrame(exam_data, index= labels)
+selected_rows = df[(df['attempts'] <2) & (df['score'] > 15)]
+print(selected_rows)   
+
+
+#12
+exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+df= pd.DataFrame(exam_data, index= labels)
+#print(df)
+print(df.iloc[df.index == 'd'])
+
+#13
+exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+df= pd.DataFrame(exam_data, index= labels)
+#getrow= (df.iloc[df.index == 'd'])
+#getrow.change(['score']== 11.5)
+#dataframe.replace("old string", "new string")
+df.loc['d', 'score'] = 11.5
+print(df)
+
